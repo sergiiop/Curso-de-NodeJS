f03afe071588e4cbb884579107310a66456a2f1f
sergio perez <sergio-luis98@hotmail.com>
2021-04-23 17:50:48 -0500

    Recibir informacion desde el cliente: body y query

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const router = express.Router();
 
 var app = express();
 
-app.use('/', function (req,res) {
-  res.send('Hola');
+// nos permite trabajar con el body de la peticion de forma sencilla
+app.use(express.json());
+app.use(router);
+
+router.get('/', function(req, res) {
+  res.send('Hola desde router get')
+});
+
+router.post('/message', function(req, res) {
+  console.log(req.body)
+  res.send('Mensaje añadido con exito')
 })
+
+
+// app.use('/', function (req,res) {
+//   res.send('Hola');
+// })
 
 app.listen(3000, function (){
   console.log('Puerto escuchando en http://localhost:3000');
